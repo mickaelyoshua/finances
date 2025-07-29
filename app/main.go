d3ea5661ab65be5549176fb36165d32f360122b8
mickaelyoshua <mickaelyoshua@gmail.com>
2025-07-29 10:50:37 -0300

    organize code

--- app/main.go
+++ app/main.go
@@ -1,52 +1,32 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/mickaelyoshua/finances/db"
+	"github.com/mickaelyoshua/finances/models"
 	"github.com/mickaelyoshua/finances/util"
 )
 
-func Test(connString string) error {
-	ctx := context.Background()
-	conn, err := db.NewConn(connString)
-	if err != nil {
-		return fmt.Errorf("Error connecting to database: %w\n", err)
-	}
-
-	result, err := conn.Query(ctx, "SELECT 1;")
-	if err != nil {
-		return fmt.Errorf("Error getting query: %w\n", err)
-	}
-	defer result.Close()
-	
-	var value int
-	for result.Next() {
-		err := result.Scan(&value)
-		if err != nil {
-			return fmt.Errorf("Error getting value from result: %w\n", err)
-		}
-		log.Printf("Result %v", value)
-	}
-
-	return nil
-}
-
 func main() {
+	// Get env variables
 	config, err := util.LoadConfig(".")
 	if err != nil {
 		log.Printf("Error loading config: %v\n", err)
 		return
 	}
-	// router := gin.Default()
-	//
-	// server := models.NewServer(router, conn)
-	// server.Router.Run()
 
-	err = Test(config.GetConnString())
+	// Get DB connection
+	conn, err := db.NewConn(config.GetConnString())
 	if err != nil {
-		log.Printf("%v\n", err)
+		log.Printf("Error connecting to database: %v\n", err)
+		return
 	}
+
+	// Run server
+	router := gin.Default()
+	
+	server := models.NewServer(router, conn)
+	server.Router.Run()
 }
