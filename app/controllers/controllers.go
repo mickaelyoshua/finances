990e202ebfb3861bc9c1a2f4ac32cd495f459b92
mickaelyoshua <mickaelyoshua@gmail.com>
2025-07-30 15:00:19 -0300

    continue with register form

--- app/controllers/controllers.go
+++ app/controllers/controllers.go
@@ -27,14 +27,54 @@ func RegisterView(ctx *gin.Context) {
 	err := Render(ctx, http.StatusOK, views.Register())
 	HandleRenderError(err)
 }
+
+func validateCreateUser(username, email, password, confirmPassword string) map[string]string {
+	errors := make(map[string]string)
+
+	if len(username) == 0 {
+		errors["username"] = "Username is required."
+	} else if len(username) < 3 {
+		errors["username"] = "Username must be at least 3 characters long."
+	}
+
+	if len(email) == 0 {
+		errors["email"] = "Email is required."
+	} else if !util.ValidEmail(email) {
+		errors["email"] = "Please provide a valid email address."
+	}
+
+	if len(password) == 0 {
+		errors["password"] = "Password is required."
+	} else if len(password) < 6 {
+		errors["password"] = "Password must be at least 6 characters long."
+	}
+
+	if len(confirmPassword) == 0 {
+		errors["confirmPassword"] = "Password confirmation is required."
+	} else if password != confirmPassword {
+		errors["confirmPassword"] = "Passwords do not match."
+	}
+
+	return errors
+}
+
 func Register(ctx *gin.Context) {
 	username := ctx.PostForm("username")
 	email := ctx.PostForm("email")
 	password := ctx.PostForm("password")
 	confirmPassword := ctx.PostForm("confirm-password")
 
-	if password != confirmPassword {
-		err := Render(ctx, http.StatusBadRequest, views.RegisterForm())
+	errors := validateCreateUser(username, email, password, confirmPassword)
+
+	if len(errors) > 0 {
+		formData := views.RegisterFormData{
+			Values: map[string]string{
+				"username": username,
+				"email":    email,
+			},
+			Errors: errors,
+		}
+		err := Render(ctx, http.StatusBadRequest, views.RegisterForm(formData))
 		HandleRenderError(err)
 		return
 	}
---
+++ app/util/util.go
@@ -0,0 +1,9 @@
+package util
+
+import "regexp"
+
+func ValidEmail(email string) bool {
+	// Regular expression for email validation
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
+	return emailRegex.MatchString(email)
+}
